21808d047ff9bf6e416c8c71ea09fb935f0d1afa
Celeste Palacios <celeste.palacios@flydevs.com>
2026-07-07 15:52:39 -0600

    Corrijo canonical URL para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://timeoverlap.app',
+  site: 'https://timeoverlap.vercel.app',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
